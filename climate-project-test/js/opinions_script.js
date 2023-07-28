4cac395eb7af31afb2d4a3bffa5dd0a7b7bf3943
grace-manthey <45363240+grace-manthey@users.noreply.github.com>
2023-07-28 16:45:05 -0700

    test URL stuff

--- climate-project-test/js/opinions_script.js
+++ climate-project-test/js/opinions_script.js
@@ -13,13 +13,20 @@ $(document).ready(function(){ // begin document.ready block
 
 	$.get('data/stories.csv', function(csvString) {
 
+			var origin = window.location.origin; 
+
 			// Use PapaParse to convert string to array of objects
 	    	var stories = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
 
 	    	console.log(stories)
 
 	    	for (i = 0; i < stories.length; i++) {
-	    		$(".carousel-row").append('<div class="carousel-tile story'+[i]+'"><a href="'+stories[i].story_link+'" target="_blank"><img src="'+stories[i].story_img+'"><div class="story-title"><p>'+stories[i].story_title+'</p></div></a></div>')
+
+	    		var link = origin + "/" + stories[i].story_link
+
+	    		console.log(link)
+
+	    		$(".carousel-row").append('<div class="carousel-tile story'+[i]+'"><a href="'+link+'" target="_blank"><img src="'+stories[i].story_img+'"><div class="story-title"><p>'+stories[i].story_title+'</p></div></a></div>')
 	    	}
 
 	    });
